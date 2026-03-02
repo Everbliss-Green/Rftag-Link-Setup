aec76f44c5d37d95e85a24cb594a13f56ee286d7
ferneediaz <dan@everblissgreen.com>
2026-03-02 15:30:30 +0800

    Deploy updates

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2126200291"
+    serviceWorkerVersion: "2650883849"
   }
 });
